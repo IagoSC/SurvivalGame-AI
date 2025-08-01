0d91ad826b93a3f9246e31cf6c5c0d8b73b6229b
iago sousa <iagosousavix@gmail.com>
2025-07-31 21:35:06 -0300

    Some changes

--- optimizer/bats_algorithm.py
+++ optimizer/bats_algorithm.py
@@ -1,96 +1,104 @@
 import math
 import numpy as np
-from typing import Callable, Optional, Tuple
 
-from dataclasses import dataclass
-
-@dataclass
-class BatsConfig:
-    space_dimensions: int = 1
-    space_lower_bound: float = -1.0
-    space_upper_bound: float = 1.0
-    population_size: int = 10
-    max_iterations: int = 1000
-    alpha: float = 0.9 #
-    gamma: float = 0.9 # Loudness decay factor
-    min_freq: float = 0.0
-    max_freq: float = 1.0
-    min_A: float = 0.0
-    max_A: float = 1.0
-    min_pulse_rate: float = 0.0
-    max_pulse_rate: float = 1.0
+np.random.seed(100)
 
 class BatsAlgorithm:
-    def __init__(self, config: BatsConfig):
-        self.config = config
-        self.population_size = config.population_size
-        self.positions = np.random.uniform(self.config.space_lower_bound, self.config.space_upper_bound,  (self.population_size, self.config.space_dimensions))
-        # Since we dont have initial best, there's no need for initial velocities
-        self.velocities = np.zeros((self.population_size, self.config.space_dimensions)) 
-        self.frequencies = np.random.uniform(config.min_freq, config.max_freq, self.population_size)
-        self.loudness = np.random.uniform(config.min_A, (config.min_A + config.max_A)/2, self.population_size)
-        self.initial_pulse_rate = np.random.uniform(config.min_pulse_rate, (config.min_pulse_rate + config.max_pulse_rate)/2, self.population_size)
-        self.pulse_rate = self.initial_pulse_rate
-        self.current_best_idx: int = -1
-        self.current_gen_fitness: np.ndarray = float('inf') * np.ones(self.population_size)
+    def __init__(self, dimensions = 8, population_size = 100, max_gen = 1000, lower_bound = -1.0, upper_bound = 1.0, min_freq = 0.0, max_freq = 1.0, min_pulse = 0.005, max_pulse = 0.5, min_A = 0.5, max_A = 1.5, alpha = 0.9, gamma = 0.9):
+        self.population_size = population_size
+        self.dimensions = dimensions
+        self.max_gen = max_gen
+        self.alpha = alpha
+        self.gamma = gamma
+        self.min_freq = min_freq
+        self.max_freq = max_freq
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
 
-    def _evaluate_population_fitness(self, fitness_function: Callable[[np.ndarray], float]) -> np.ndarray:
-        self.current_gen_fitness = [fitness_function(position) for i, position in enumerate(self.positions)]
-        self.current_best_idx = np.argmin(self.current_gen_fitness).__int__()
+        self.positions = np.random.uniform(lower_bound, upper_bound,  (population_size, dimensions))
+        self.velocities = np.zeros((population_size, dimensions)) 
+        self.frequencies = np.random.uniform(min_freq, max_freq, population_size)
+        self.loudness = np.random.uniform(min_A, max_A, population_size)
+        self.initial_pulse_rate = np.random.uniform(min_pulse, max_pulse, population_size)
+        self.pulse_rate = self.initial_pulse_rate
+
+    def _evaluate_population_fitness(self, fitness_function) -> np.ndarray:
+        self.current_gen_fitness = fitness_function(self.positions)
+        self.current_best_idx = np.argmax(self.current_gen_fitness)
         
     def _update_frequencies(self):
         beta = np.random.uniform(0, 1, self.population_size)
-        self.frequencies = np.clip(self.config.min_freq + (self.config.max_freq - self.config.min_freq) * beta)
+        self.frequencies = self.min_freq + (self.max_freq - self.min_freq) * beta
 
-    def _update_velocities(self, current_best_position: np.ndarray, alpha: float):
+    def _update_velocities(self, current_best_position: np.ndarray):
         direction = (current_best_position - self.positions)
         self.velocities += direction * self.frequencies[:, np.newaxis]
 
     def _update_positions(self):
         self.positions += self.velocities
-        self.positions = np.clip(self.positions, self.config.space_lower_bound, self.config.space_upper_bound)
+        self.positions = np.clip(self.positions, self.lower_bound, self.upper_bound)
 
     def _update_loudness(self):
-        self.loudness *= self.config.alpha
+        self.loudness *= self.alpha
 
     def _update_pulse_rate(self, current_iteration: int):
-        self.pulse_rate = self.pulse_rate * (1 - (math.exp(-self.config.gamma * current_iteration)))
+        self.pulse_rate = self.initial_pulse_rate * (1 - (math.exp(-self.gamma * current_iteration)))
 
-    def _fly_randomly(self, position: np.ndarray, epsilon: float, avg_loudness: float) -> np.ndarray:
-        return np.clip(position + epsilon * avg_loudness, self.config.space_lower_bound, self.config.space_upper_bound)
+    def _fly_randomly(self, best_position: np.ndarray, current_positions: np.ndarray, avg_loudness: float) -> np.ndarray:
+        rand = np.random.uniform(0,1, self.population_size)
+        is_moving = np.greater(rand,  self.pulse_rate)
 
-    def optimize(self, fitness_function: Callable[[np.ndarray], float]) -> np.ndarray:
+        epsilon = np.where(
+            is_moving[:, np.newaxis],
+            np.random.uniform(-1, 1, current_positions.shape) * avg_loudness,
+            np.zeros(current_positions.shape)
+        )
+        best_positions = np.array([best_position] * self.population_size)
+        return np.clip(best_positions +  epsilon, self.lower_bound, self.upper_bound)
+
+    def optimize(self, fitness_function) -> np.ndarray:
         self._evaluate_population_fitness(fitness_function)
         
-        for gen in range(self.config.max_iterations):
+        for gen in range(self.max_gen):
             best_position = self.positions[self.current_best_idx]
             self._update_frequencies()
-            self._update_velocities(best_position, self.config.alpha)
+            self._update_velocities(best_position)
             self._update_positions()
             self._evaluate_population_fitness(fitness_function)
-            
+
+            # TODO 
+            # 1 - Try move anyway
+            # 2 - Try move only if fitness is better and loudnes check
+
             avg_loudness = np.mean(self.loudness)
-            
-            for i in range(self.population_size):
-                epsilon = np.random.uniform(-1, 1, self.config.space_dimensions)
 
-                if np.random.uniform(0,1) > self.pulse_rate[i]:
-                    # Search around global best
-                    new_position = self._fly_randomly(best_position, epsilon, avg_loudness)
-                else:
-                    # Local search
-                    new_position = self._fly_randomly(self.positions[i], epsilon, avg_loudness)
+            new_positions = self._fly_randomly(best_position, self.positions, avg_loudness)
 
-                new_fitness = fitness_function(new_position)
-                if new_fitness <= self.current_gen_fitness[self.current_best_idx] and np.random.uniform(0, 1) < self.loudness[i]:
-                    self.positions[i] = new_position
-                    self.current_gen_fitness[i] = new_fitness
-                    
-                    if new_fitness < self.current_gen_fitness[self.current_best_idx]:
-                        self.current_best_idx = i
-                
-                self.loudness[i] *= self.config.alpha
-                self.pulse_rate[i] = np.random.uniform(self.config.min_pulse_rate, self.config.max_pulse_rate)
+            new_fitness = fitness_function(new_positions)
 
+            is_better = np.greater_equal(new_fitness, self.current_gen_fitness)
+            is_below_loudness = np.less(np.random.uniform(0, 1, self.population_size), self.loudness)
 
-        return self.positions[self.current_best_idx]
+            update_positions = np.logical_and(is_better, is_below_loudness)
+
+            self.positions = np.where(
+                update_positions[:, np.newaxis],
+                new_positions,
+                self.positions
+            )
+
+            self.current_gen_fitness = np.where(
+                update_positions,
+                new_fitness,
+                self.current_gen_fitness
+            )
+
+            self._update_loudness()
+            self._update_pulse_rate(gen)
+            self.current_best_idx = np.argmax(self.current_gen_fitness)
+            print(f"Generation {gen + 1}")
+            print(f"Best fitness {self.current_gen_fitness[self.current_best_idx]}")
+            print(f"Avg fitness {np.mean(self.current_gen_fitness)}")
+            print(f"Std fitness {np.std(self.current_gen_fitness)}")
+
+        return self.positions[self.current_best_idx], self.current_gen_fitness[self.current_best_idx]
